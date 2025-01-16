191567ee73a1b864687fdc4d3871382cd091d5e7
degenicus <degenicus@bytemasons.com>
2025-01-16 17:35:02 +0100

    Update constants file with testnet deployer address

--- scripts/config/constants.ts
+++ scripts/config/constants.ts
@@ -2,21 +2,21 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export const adminCapOwner = {
-    "mainnet": "0xd0ec0b201de6b4e7f425918bbd7151c37fc1b06c59b3961a2a00db74f6ea865e",
-    "testnet": "0xb3d277c50f7b846a5f609a8d13428ae482b5826bb98437997373f3a0d60d280e",
+    "mainnet": "",
+    "testnet": "0x69d0ed97ad0620655ed5877f397f28d561e09b97f27f876a846b680ea97e3efc",
 };
 
 export const upgradeCapOwner = {
-    "mainnet": "0x37f187e1e54e9c9b8c78b6c46a7281f644ebc62e75493623edcaa6d1dfcf64d2",
-    "testnet": "0xb3d277c50f7b846a5f609a8d13428ae482b5826bb98437997373f3a0d60d280e",
+    "mainnet": "",
+    "testnet": "0x69d0ed97ad0620655ed5877f397f28d561e09b97f27f876a846b680ea97e3efc",
 };
 
 export const upgradeCapID = {
-    "mainnet": "0xdadf253cea3b91010e64651b03da6d56166a4f44b43bdd4e185c277658634483",
-    "testnet": "0x56632f4f1b707b5b0c1b99defe65a056af3f846d95700b4a1cbd3adc94bf7c96",
+    "mainnet": "",
+    "testnet": "0xdd0d6086d53ebf641e7e80bbb91f9fcca4992b6e04d3c46aaf98d9365745be0a",
 };
 
 export const adminCapID = {
-    "mainnet": "0xada554b8b712556b8509be47ac1bc04db9505c3532049a543721aca0c010a840",
-    "testnet": "0x014e6a65f60936177820141ad64430290b6ad5e16421691dc9f3fa9907154b2e"
+    "mainnet": "",
+    "testnet": "0xc1117e3adfa36d54c7ceb21e80ca7e8e206374441d9418032d16c513e4da2a8e"
 };
