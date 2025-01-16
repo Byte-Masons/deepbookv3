dca94ed38b0c06d8fd013eabde8e74eb721f0b11
degenicus <degenicus@bytemasons.com>
2025-01-16 17:39:53 +0100

    Change createPool script to use private key from .env directly instead of multisig

--- scripts/transactions/createPool.ts
+++ scripts/transactions/createPool.ts
@@ -1,14 +1,32 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 import { Transaction } from "@iota/iota-sdk/transactions";
-import { prepareMultisigTx } from "../utils/utils";
-import { adminCapOwner, adminCapID } from "../config/constants";
+import { adminCapID } from "../config/constants";
 import { DeepBookClient } from "deepbook-v3-iota";
-import { getFullnodeUrl, IotaClient } from "@iota/iota-sdk/client";
+import { IotaClient } from "@iota/iota-sdk/client";
+import { decodeIotaPrivateKey } from '@iota/iota-sdk/cryptography';
+import { Ed25519Keypair } from '@iota/iota-sdk/keypairs/ed25519';
+import type { Keypair } from '@iota/iota-sdk/cryptography';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const getSignerFromPK = (privateKey: string) => {
+  const { schema, secretKey } = decodeIotaPrivateKey(privateKey);
+  if (schema === 'ED25519') return Ed25519Keypair.fromSecretKey(secretKey);
+
+  throw new Error(`Unsupported schema: ${schema}`);
+};
 
 (async () => {
   // Update constant for env
   const env = "testnet";
+
+  const privateKey = process.env.PRIVATE_KEY;
+  // console.log(process.env);
+	if (!privateKey) {
+		throw new Error('Private key not found');
+	}
+  console.log(privateKey);
 
   // Initialize with balance managers if needed
   const balanceManagers = {
@@ -35,8 +53,8 @@ import { getFullnodeUrl, IotaClient } from "@iota/iota-sdk/client";
   const tx = new Transaction();
 
   dbClient.deepBookAdmin.createPoolAdmin({
-    baseCoinKey: "TYPUS",
-    quoteCoinKey: "SUI",
+    baseCoinKey: "DEEP",
+    quoteCoinKey: "IOTA",
     tickSize: 0.00001,
     lotSize: 0.1,
     minSize: 1,
@@ -44,7 +62,16 @@ import { getFullnodeUrl, IotaClient } from "@iota/iota-sdk/client";
     stablePool: false,
   })(tx);
 
-  let res = await prepareMultisigTx(tx, env, adminCapOwner[env]);
+  let resolvedKeypair: Keypair = getSignerFromPK(privateKey);
 
-  console.dir(res, { depth: null });
+  const response = await dbClient.client.signAndExecuteTransaction({
+    transaction: tx,
+    signer: resolvedKeypair,
+    options: {
+      showEffects: true,
+      showObjectChanges: true,
+    }
+  });
+
+  console.dir(response, { depth: null });
 })();
